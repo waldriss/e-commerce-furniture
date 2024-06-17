e2615a80e62161d64dbba5986aa188ff04c9c11f
walid <idrisswalid10@gmail.com>
2024-06-17 20:45:44 +0100

    updates

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const {OAuth2Client} = require('google-auth-library');
