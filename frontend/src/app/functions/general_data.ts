6d817cf6162cb9e8b36826898724b30afd804b49
walid <idrisswalid10@gmail.com>
2024-06-18 15:30:53 +0100

    fixing bugs

--- frontend/src/app/functions/general_data.ts
+++ frontend/src/app/functions/general_data.ts
@@ -1,5 +1,5 @@
 
-export const backendUrl="https://e-commerce-backend.fly.dev"
+export const backendUrl="https://e-commerce-backend.fly.dev/"
 //export const backendUrl="https://backend-e-com-w.onrender.com/"
 //export const backendUrl="http://localhost:8000/"
 
