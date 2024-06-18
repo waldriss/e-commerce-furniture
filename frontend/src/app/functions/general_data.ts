4dabd4aafc53461bc49a41f67e028e5720b87d6b
walid <idrisswalid10@gmail.com>
2024-06-18 14:18:02 +0100

    updates front to host on vercel

--- frontend/src/app/functions/general_data.ts
+++ frontend/src/app/functions/general_data.ts
@@ -1,6 +1,6 @@
 
-
-export const backendUrl="https://backend-e-com-w.onrender.com/"
+export const backendUrl="https://e-commerce-backend.fly.dev"
+//export const backendUrl="https://backend-e-com-w.onrender.com/"
 //export const backendUrl="http://localhost:8000/"
 
 
